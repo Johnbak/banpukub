fdd4f504b20546605190276f5ca18e63ffcb3b5b
tik <najaroen_tik@live.com>
2021-03-24 14:05:56 +0700

    add test and cal pwr case 1 file

--- src/controller/FileController.ts
+++ src/controller/FileController.ts
@@ -3,43 +3,55 @@ import { NextFunction, Request, Response } from 'express';
 import HttpException from '../common/http-exception';
 import { FileUpload } from '../types/FileUpload';
 import { operationService } from '../services/OperationService';
+
 export class FileController {
 
-  getFile = (req:Request, res:Response, next:NextFunction) => {
+  getFile = (req: Request, res: Response, next: NextFunction) => {
     try {
       res.status(200).json({
-        message:'Success'
-      })
+        message: 'Success'
+      });
     } catch (e) {
-        const errors:HttpException = new HttpException(400, e.message, e)
-        next(errors)
+      const errors: HttpException = new HttpException(400, e.message, e);
+      next(errors);
     }
 
-  }
+  };
 
-  upload = async (req:Request, res:Response, next:NextFunction) => {
+  upload = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const file:any = req.files;
+      const file: any = req.files;
       const listKey = Object.keys(file);
-      let list:FileUpload[] = [];
+      let list: FileUpload[] = [];
       for (const name of listKey) {
-        const fileUpload : FileUpload =  {
-          name:file[name].name,
-          data:file[name].data,
-          encoding:file[name].encoding,
-          md5:file[name].md5,
-          mimetype:file[name].mimetype,
-          tempFilePath:'',
-          truncated:file[name].truncated,
-          mv:file[name].mv,
-          size:file[name].size
-        }
+        const fileUpload: FileUpload = {
+          name: file[name].name,
+          data: file[name].data,
+          encoding: file[name].encoding,
+          md5: file[name].md5,
+          mimetype: file[name].mimetype,
+          tempFilePath: '',
+          truncated: file[name].truncated,
+          mv: file[name].mv,
+          size: file[name].size
+        };
         list.push(fileUpload);
       }
-      const result = await operationService.uploadFile(list, '2021-06-22');
-      res.send(result)
+      const {
+        listItemRadiation,
+        listItemPowerGeneration,
+        listItemRadiationPreview,
+        listItemPowerGenerationPreview
+      } = await operationService.uploadFile(list, '2021-06-22');
+
+      res.status(200).json({
+        listItemPowerGeneration,
+        listItemPowerGenerationPreview,
+        listItemRadiation,
+        listItemRadiationPreview
+      });
     } catch (e) {
-      next(e)
+      next(e);
     }
-  }
+  };
 }
